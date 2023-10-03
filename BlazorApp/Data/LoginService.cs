603df21a6a81bea3326aefe61af0ba9429fabafc
jbackus1 <jbackus1@uw.edu>
2023-10-03 15:57:31 -0700

    Finished with the Course

---
+++ BlazorApp/Data/LoginService.cs
@@ -0,0 +1,24 @@
+using System;
+using static System.Console;
+
+namespace BlazorApp.Data
+{
+    public class LoginService
+    {
+        private int loginstat;
+        public void setlogin(int login)
+        {
+            this.loginstat = login;
+        }
+
+        public int getlogin()
+        {
+            return this.loginstat;
+        }
+
+        public Task<MemberData> GetMemberInfo()
+        {
+            return Task.FromResult(new MemberData{Name = "Kim"});
+        }
+    }
+}
---
+++ BlazorApp/Data/MemberData.cs
@@ -0,0 +1,10 @@
+using System;
+using static System.Console;
+
+namespace BlazorApp.Data
+{
+    public class MemberData
+    {
+        public string Name {get; set;}
+    }
+}
